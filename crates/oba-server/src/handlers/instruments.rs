35c001885469f5c480e6d79bc6681ddfd3e73bcc
udsamani <uditpsamani@hotmail.com>
2024-07-07 00:33:42 +0100

    remove unnecesary folders in handlers

--- crates/oba-server/src/handlers/instruments.rs
+++ crates/oba-server/src/handlers/instruments.rs
@@ -1,8 +1,10 @@
 use axum::{extract::{Path, State}, Json};
+use tracing::info;
 
 use crate::AppState;
 
-use super::{OrderBookError, OrderBookResponse};
+use super::{OrderBookError, OrderBookResponse, SubscribeInstrumentRequest, UnsubscribeInstrumentRequest};
+
 
 pub async fn get_order_book(State(state): State<AppState>, Path(instrument_name): Path<String>) -> Result<Json<OrderBookResponse>, OrderBookError>{
     let order_book_manager = state.order_book_manager.read().await;
@@ -21,7 +23,22 @@ pub async fn get_order_book(State(state): State<AppState>, Path(instrument_name)
             Err(OrderBookError::NotFound(instrument_name))
         }
     }
+}
 
-    
-    
+pub async fn subscribe_instrument(
+    State(state): State<AppState>,
+    Json(request): Json<SubscribeInstrumentRequest>,
+) {
+    info!("Subscribe to {:?}", request);
+    let mut order_book_manager = state.order_book_manager.write().await;
+    order_book_manager.subscribe_instrument(request.name, request.order_book_depth);
+}
+
+pub async fn unsubscribe_instrument(
+    State(state): State<AppState>,
+    Json(request): Json<UnsubscribeInstrumentRequest>) {
+
+    info!("Unsubscribe  {:?}", request);
+    let mut order_book_manager = state.order_book_manager.write().await;
+    order_book_manager.unsubscribe_instrument(request.name);
 }
--- crates/oba-server/src/handlers/mod.rs
+++ crates/oba-server/src/handlers/mod.rs
@@ -1,1 +1,53 @@
+use std::collections::BTreeMap;
+
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
 pub mod instruments;
+
+#[derive(Debug, Deserialize)]
+pub struct SubscribeInstrumentRequest {
+    name: String,
+    order_book_depth: usize,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UnsubscribeInstrumentRequest {
+    name: String,
+}
+
+type Type = BTreeMap<u64, f64>;
+
+#[derive(Debug, Serialize)]
+pub struct OrderBookResponse {
+    symbol: String,
+    bids: BTreeMap<u64, f64>,
+    asks: Type,
+}
+
+#[derive(Debug)]
+pub enum OrderBookError {
+    InternalServerError,
+    NotFound(String),
+}
+
+impl IntoResponse for OrderBookError {
+    fn into_response(self) -> axum::response::Response {
+        let (status, error_msg) = match self {
+            Self::NotFound(instrument) => (
+                StatusCode::NOT_FOUND,
+                format!(
+                    "Instrument with name {} not found in order books",
+                    instrument
+                ),
+            ),
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                String::from("Internal Server Error"),
+            ),
+        };
+
+        (status, Json(json!({"error_message": error_msg}))).into_response()
+    }
+}
