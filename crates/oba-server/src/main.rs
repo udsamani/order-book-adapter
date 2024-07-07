1ff4310e9b0663792a942b83ca8adc3950226fe9
udsamani <uditpsamani@hotmail.com>
2024-07-07 17:29:10 +0100

    fix default logging

--- crates/oba-server/src/main.rs
+++ crates/oba-server/src/main.rs
@@ -58,8 +58,11 @@ async fn main() {
 }
 
 fn init_tracing() {
+
     tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::try_from_default_env().unwrap())
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| "info".into()),
+        )
         .with(tracing_subscriber::fmt::layer())
         .init();
 }
