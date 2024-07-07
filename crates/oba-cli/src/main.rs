7ffc37009a8068086378f6e797e04a458e030327
udsamani <uditpsamani@hotmail.com>
2024-07-07 16:44:22 +0100

    fix few things

--- crates/oba-cli/src/main.rs
+++ crates/oba-cli/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
+use clap::Parser;
+use cli::{OBACli, OBASubCommands};
+use get_order_book::get_order_books;
+use subscribe::subscribe_instrument;
+use unsubscribe::unsubscribe_instrument;
+
+pub mod cli;
+pub mod subscribe;
+pub mod get_order_book;
+pub mod unsubscribe;
+
+#[tokio::main]
+async fn main() {
+    let cli = OBACli::parse();
+
+    match &cli.command {
+        OBASubCommands::Subscribe { instrument, depth } => {
+            println!("Here");
+            subscribe_instrument(instrument, depth).await;            
+        },
+        OBASubCommands::GetOrderBook { instrument } => {
+           get_order_books(instrument).await;
+        },
+        OBASubCommands::Unsubscribe { instrument } => {
+            unsubscribe_instrument(instrument).await;
+            
+        }
+        
+    }
     println!("Welcome to OBA CLI")
 }
